ab529ad9fd3e7a05e41c3a3cbc2b0517301ea050
nayemsabur <mdsabur9991@gmail.com>
2026-03-11 08:56:48 +0600

    Match lamp to classic shade reference

--- src/components/LampModel.jsx
+++ src/components/LampModel.jsx
@@ -2,15 +2,17 @@ export default function LampModel({ lampOn }) {
   return (
     <div className={`lamp-illustration ${lampOn ? "is-on" : "is-off"}`} aria-hidden="true">
       <div className="lamp-shade">
+        <span className="shade-top" />
+        <span className="shade-bottom" />
         <span className="eye eye-left" />
         <span className="eye eye-right" />
         <span className="mouth" />
       </div>
+      <div className="lamp-shade-inner" />
       <div className="lamp-glow" />
-      <div className="lamp-leg lamp-leg-left" />
-      <div className="lamp-leg lamp-leg-right" />
-      <div className="lamp-stem" />
+      <div className="lamp-pole" />
       <div className="lamp-base" />
+      <div className="lamp-table-top" />
     </div>
   );
 }
